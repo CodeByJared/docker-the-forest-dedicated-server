b91bd85ec97d477d0282ef0a0e88ab99f907f446
Jared Linares <51540063+CodeByJared@users.noreply.github.com>
2025-01-03 17:25:43 -0500

    update

--- Dockerfile
+++ Dockerfile
@@ -10,7 +10,31 @@ ENV WINEPREFIX=/winedata/WINE64 \
     PUID=0 \
     PGID=0 \
     SERVER_STEAM_ACCOUNT_TOKEN="" \
-    ALWAYS_UPDATE_ON_START=1
+    ALWAYS_UPDATE_ON_START=1 \
+    SERVER_IP="" \
+    SERVER_STEAM_PORT="8766" \
+    SERVER_GAME_PORT="27015" \
+    SERVER_QUERY_PORT="27016" \
+    SERVER_PLAYERS="8" \
+    SERVER_VAC="off" \
+    SERVER_PASSWORD="" \
+    SERVER_ADMIN_PASSWORD="" \
+    SERVER_AUTO_SAVE_INTERVAL="30" \
+    SERVER_DIFFICULTY="Normal" \
+    SERVER_INIT_TYPE="Continue" \
+    SERVER_SLOT="1" \
+    SERVER_SHOW_LOGS="off" \
+    SERVER_CONTACT="email@gmail.com" \
+    SERVER_VEGAN_MODE="off" \
+    SERVER_VEGETARIAN_MODE="off" \
+    SERVER_RESET_HOLES_MODE="off" \
+    SERVER_TREE_REGROW_MODE="off" \
+    SERVER_ALLOW_BUILDING_DESTRUCTION="on" \
+    SERVER_ALLOW_ENEMIES_CREATIVE="off" \
+    SERVER_ALLOW_CHEATS="off" \
+    SERVER_REALISTIC_PLAYER_DAMAGE="off" \
+    SERVER_TARGET_FPS_IDLE="0" \
+    SERVER_TARGET_FPS_ACTIVE="0"
 
 VOLUME ["/theforest", "/steamcmd", "/winedata"]
 
@@ -18,7 +42,7 @@ EXPOSE 8766/tcp 8766/udp 27015/tcp 27015/udp 27016/tcp 27016/udp
 
 RUN dpkg --add-architecture i386 \
     && apt-get update \
-    && apt-get install -y --no-install-recommends --no-install-suggests lib32gcc-s1 winbind xvfb \
+    && apt-get install -y --no-install-recommends --no-install-suggests ping lib32gcc-s1 winbind xvfb \
     && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
 COPY ./usr/bin/servermanager.sh /usr/bin/servermanager.sh
--- usr/bin/servermanager.sh
+++ usr/bin/servermanager.sh
@@ -56,16 +56,44 @@ function startVirtualScreenAndRebootWine() {
 
 function installServer() {
     RANDOM_NUMBER=$RANDOM
-    # force a fresh install of all
     echo ">>> Forcing a fresh server install"
     echo "> Setting server-name to jammsen-docker-generated-$RANDOM_NUMBER"
     isWineinBashRcExistent
     steamcmdinstaller.sh
     mkdir -p $SAVEGAME_PATH $CONFIG_PATH
     cp /server.cfg.example $CONFIGFILE_PATH
+    
+    # Set environment variables with defaults
+    SERVER_IP="${SERVER_IP:-$(hostname -I)}"
+    
+    # Update config file with environment variables
+    sed -i -e "s/^serverIP .*/serverIP $SERVER_IP/g" $CONFIGFILE_PATH
+    sed -i -e "s/^serverSteamPort .*/serverSteamPort $SERVER_STEAM_PORT/g" $CONFIGFILE_PATH
+    sed -i -e "s/^serverGamePort .*/serverGamePort $SERVER_GAME_PORT/g" $CONFIGFILE_PATH
+    sed -i -e "s/^serverQueryPort .*/serverQueryPort $SERVER_QUERY_PORT/g" $CONFIGFILE_PATH
+    sed -i -e "s/^serverPlayers .*/serverPlayers $SERVER_PLAYERS/g" $CONFIGFILE_PATH
+    sed -i -e "s/^enableVAC .*/enableVAC $SERVER_VAC/g" $CONFIGFILE_PATH
+    sed -i -e "s/^serverPassword .*/serverPassword $SERVER_PASSWORD/g" $CONFIGFILE_PATH
+    sed -i -e "s/^serverPasswordAdmin .*/serverPasswordAdmin $SERVER_ADMIN_PASSWORD/g" $CONFIGFILE_PATH
+    sed -i -e "s/^serverAutoSaveInterval .*/serverAutoSaveInterval $SERVER_AUTO_SAVE_INTERVAL/g" $CONFIGFILE_PATH
+    sed -i -e "s/^difficulty .*/difficulty $SERVER_DIFFICULTY/g" $CONFIGFILE_PATH
+    sed -i -e "s/^initType .*/initType $SERVER_INIT_TYPE/g" $CONFIGFILE_PATH
+    sed -i -e "s/^slot .*/slot $SERVER_SLOT/g" $CONFIGFILE_PATH
+    sed -i -e "s/^showLogs .*/showLogs $SERVER_SHOW_LOGS/g" $CONFIGFILE_PATH
+    sed -i -e "s/^serverContact .*/serverContact $SERVER_CONTACT/g" $CONFIGFILE_PATH
+    sed -i -e "s/^veganMode .*/veganMode $SERVER_VEGAN_MODE/g" $CONFIGFILE_PATH
+    sed -i -e "s/^vegetarianMode .*/vegetarianMode $SERVER_VEGETARIAN_MODE/g" $CONFIGFILE_PATH
+    sed -i -e "s/^resetHolesMode .*/resetHolesMode $SERVER_RESET_HOLES_MODE/g" $CONFIGFILE_PATH
+    sed -i -e "s/^treeRegrowMode .*/treeRegrowMode $SERVER_TREE_REGROW_MODE/g" $CONFIGFILE_PATH
+    sed -i -e "s/^allowBuildingDestruction .*/allowBuildingDestruction $SERVER_ALLOW_BUILDING_DESTRUCTION/g" $CONFIGFILE_PATH
+    sed -i -e "s/^allowEnemiesCreativeMode .*/allowEnemiesCreativeMode $SERVER_ALLOW_ENEMIES_CREATIVE/g" $CONFIGFILE_PATH
+    sed -i -e "s/^allowCheats .*/allowCheats $SERVER_ALLOW_CHEATS/g" $CONFIGFILE_PATH
+    sed -i -e "s/^realisticPlayerDamage .*/realisticPlayerDamage $SERVER_REALISTIC_PLAYER_DAMAGE/g" $CONFIGFILE_PATH
+    sed -i -e "s/^targetFpsIdle .*/targetFpsIdle $SERVER_TARGET_FPS_IDLE/g" $CONFIGFILE_PATH
+    sed -i -e "s/^targetFpsActive .*/targetFpsActive $SERVER_TARGET_FPS_ACTIVE/g" $CONFIGFILE_PATH
     sed -i -e "s/###serverSteamAccount###/$SERVER_STEAM_ACCOUNT_TOKEN/g" $CONFIGFILE_PATH
     sed -i -e "s/###RANDOM###/$RANDOM_NUMBER/g" $CONFIGFILE_PATH
-    sed -i -e "s/[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}/$(hostname -I)/g" $CONFIGFILE_PATH
+    
     bash /steamcmd/steamcmd.sh +runscript /steamcmdinstall.txt
 }
 
